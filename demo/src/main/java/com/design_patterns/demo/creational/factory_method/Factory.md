709bb1a3f2267f82220c7dff86873256866837f5
atther <net.ather@gmail.com>
2025-02-27 23:52:26 +0530

    builder

--- demo/src/main/java/com/design_patterns/demo/creational/factory_method/Factory.md
+++ demo/src/main/java/com/design_patterns/demo/creational/factory_method/Factory.md
@@ -6,3 +6,5 @@ of the instantiation of a class from the client program to the factory class. Le
 implement a factory design pattern in java and then we will look into factory pattern advantages.
 We will see some of the factory design pattern usage in JDK. Note that this pattern is also known 
 as Factory Method Design Pattern.
+* https://www.digitalocean.com/community/tutorials/factory-design-pattern-in-java
+* https://github.com/iluwatar/java-design-patterns/blob/master/factory-method/src/test/java/com/iluwatar/factory/method/FactoryMethodTest.java
---
+++ demo/src/test/java/com/design_patterns/demo/creational/factory_method/FactoryTest.java
@@ -0,0 +1,21 @@
+package com.design_patterns.demo.creational.factory_method;
+
+
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+public class FactoryTest {
+
+    public static void main(String[] args) {
+        Computer pc1 = ComputerFactory.getComputer("pc","2 GB","500 GB","2.4 GHz");
+        Computer pc2 = ComputerFactory.getComputer("pc","2 GB","500 GB","2.4 GHz");
+
+        Computer server1 = ComputerFactory.getComputer("server","16 GB","1 TB","2.9 GHz");
+        Computer server2 = ComputerFactory.getComputer("server","32 GB","1 TB","2.9 GHz");
+        System.out.println("Factory PC Config::"+pc1);
+        System.out.println("Factory Server Config::"+server1);
+
+        assertSame(pc1, pc2);
+        assertSame(server1, server2);
+    }
+
+}
